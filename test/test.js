01707bd4202e421e5a4192b03f79a45b6f5d0c50
Cathy Dutton <info@cathydutton.co.uk>
2016-02-22 23:05:53 +0000

    JS Test

--- test/test.js
+++ test/test.js
@@ -10,6 +10,6 @@ var test = function (opts, input, output) {
     assert.equal(postcss(grid(opts)).process(input).css, output);
 };
 
-test(settingsConfig, '.element{col-span: mobile(1,1);}', '.element{float: left;width: 99%;margin-right: 0.5%;margin-left: 0.5%;}');
-test(settingsConfig, '.element{col-span: mobile(1,2);}', '.element{float: left;width: 49%;margin-right: 0.5%;margin-left: 0.5%;}');
-test(settingsConfig, '.element{col-span: mobile(1,3);}', '.element{float: left;width: 32.333333333333336%;margin-right: 0.5%;margin-left: 0.5%;}');
+test(settingsConfig, '.element{col-span: mobile(1,1);}', '.element{float: left;box-sizing: border-box;margin-right: 0.5%;margin-left: 0.5%;width: 99%;transition: all 0.5s ease;}');
+test(settingsConfig, '.element{col-span: mobile(1,2);}', '.element{float: left;box-sizing: border-box;margin-right: 0.5%;margin-left: 0.5%;width: 49%;transition: all 0.5s ease;}');
+test(settingsConfig, '.element{col-span: mobile(1,3);}', '.element{float: left;box-sizing: border-box;margin-right: 0.5%;margin-left: 0.5%;width: 32.333333333333336%;transition: all 0.5s ease;}');
